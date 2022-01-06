35cc3089da3ee55f9828dd9c256fd613c5f9d832
Horsmanayo <oniademola001@gmail.com>
2022-01-06 22:56:45 +0100

    first-update

--- src/App.js
+++ src/App.js
@@ -4,26 +4,45 @@ import { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
   
-    this.state = {
-       fullName: '',
-       bio: '',
+    state = {
+       fullName: 'Oni Usman Ademola',
+       bio: 'A full stack student at Gomycode Nigeria',
        imgSrc :'',
-       profession :'',
+       profession :'Full Stack Junior Developer',
        shows : false
 
     }
-  }
+
+    display =() => {
+      this.setState(prevState => {
+        return {
+          shows : prevState.shows === true? false : true
+        }
+      })
+    }
+
 
   render(){
+    if(this.state.shows === true){
+      return (
+        <div className="App">
+            <button onClick = {this.display}>Log out</button>
+            <h1>{this.state.fullName}</h1>
+            <h1>{this.state.bio}</h1>
+            <h1>{this.state.profession}</h1>
+        </div>
+      )
+    }else{
+      return (
+        <div className="App">
+          <button onClick = {this.display}>Log in</button>
+          <h1>This user is not logged in </h1>
+        </div>
+      )
+    }
     
-    return (
-      <div className="App">
-        
-      </div>
-    );
+    
   }
   
 }
